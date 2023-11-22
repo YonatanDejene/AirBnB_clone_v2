f72eb5b7e964486fc737e4fdfdd1c285657840e1
YonatanDejene <33670253+YonatanDejene@users.noreply.github.com>
2023-11-22 18:11:28 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -10,7 +10,7 @@ class Amenity(BaseModel, Base):
     Attributes:
         name: input name
     """
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
+    __tablename__ = 'amenities'
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
