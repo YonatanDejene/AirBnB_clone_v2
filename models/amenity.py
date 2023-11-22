3ac35bdc985656eb7f93a36438ffa54100ebc997
YonatanDejene <33670253+YonatanDejene@users.noreply.github.com>
2023-11-22 17:04:41 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Defines the Amenity class."""
-from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column
 from sqlalchemy import String
