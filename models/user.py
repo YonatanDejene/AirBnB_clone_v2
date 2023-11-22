dc1dcff60c806279da01901c75e4fd7f7c082984
YonatanDejene <33670253+YonatanDejene@users.noreply.github.com>
2023-11-22 18:21:47 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import Base
 from models.base_model import BaseModel
+from models.base_model import Base
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
